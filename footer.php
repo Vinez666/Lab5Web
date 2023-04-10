54985e162d467b6815178a70194d7efd4e951704
Vinez666 <127708664+Vinez666@users.noreply.github.com>
2023-04-10 09:43:12 +0700

    commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+  <p>&copy; 2023, Teknik Informatika, <a href="https://www.youtube.com/channel/UCuxdnl1N-2TvXRfxq1fAKAA" style="text-decoration: none;">Kevin Suyadi</a>, Universitas Pelita Bangsa</p>
+</footer>
+</div>
+</body>
+
+</html>
